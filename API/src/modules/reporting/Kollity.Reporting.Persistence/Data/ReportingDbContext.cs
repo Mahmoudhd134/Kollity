788116cb1fb72ad710d0a053432709c2a95dde50
Mahmoud Nasser <nassermahmoud571@gmail.com>
2024-04-18 01:28:51 +0200

    Refactoring & Init database config

---
+++ API/src/modules/reporting/Kollity.Reporting.Persistence/Data/ReportingDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Kollity.Reporting.Persistence.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kollity.Reporting.Persistence.Data;
+
+public class ReportingDbContext : DbContext
+{
+    public ReportingDbContext(DbContextOptions<ReportingDbContext> options) : base(options)
+    {
+    }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSnakeCaseNamingConvention();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("services");
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(PersistenceExtensions).Assembly);
+        builder.ApplyUtcDateTimeConverter();
+    }
+}
