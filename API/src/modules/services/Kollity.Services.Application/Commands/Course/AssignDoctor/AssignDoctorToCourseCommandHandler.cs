3560247d70237f34ec5b5e3ff6a90c8d1c0580f0
Mahmoud Nasser <nassermahmoud571@gmail.com>
2024-05-11 17:57:06 +0300

    fix-bug/services-service fix assign doctor to course command

--- API/src/modules/services/Kollity.Services.Application/Commands/Course/AssignDoctor/AssignDoctorToCourseCommandHandler.cs
+++ API/src/modules/services/Kollity.Services.Application/Commands/Course/AssignDoctor/AssignDoctorToCourseCommandHandler.cs
@@ -23,6 +23,9 @@ public class AssignDoctorToCourseCommandHandler : ICommandHandler<AssignDoctorTo
         var doctor = await _context.Doctors.FirstOrDefaultAsync(x => x.Id == doctorId, cancellationToken);
         if (doctor is null)
             return DoctorErrors.IdNotFound(doctorId);
+        
+        if (doctor.UserType != UserType.Doctor)
+            return CourseErrors.NonDoctorAssignation;
 
         var course = await _context.Courses.FirstOrDefaultAsync(x => x.Id == courseId, cancellationToken);
         if (course is null)
@@ -30,11 +33,7 @@ public class AssignDoctorToCourseCommandHandler : ICommandHandler<AssignDoctorTo
 
         if (course.DoctorId != null)
             return CourseErrors.HasAnAssignedDoctor;
-
-        var isInDoctorRole = _userServices.IsInRole(Role.Doctor);
-        if (isInDoctorRole == false)
-            return CourseErrors.NonDoctorAssignation;
-
+        
         course.DoctorId = doctorId;
         var result = await _context.SaveChangesAsync(cancellationToken);
         return result > 0 ? Result.Success() : Error.UnKnown;
